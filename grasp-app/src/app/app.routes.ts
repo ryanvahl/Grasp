b18d8062b42c05fb11c8097e5646bd908893d044
Ryan <ryanvahl@gmail.com>
2024-11-11 03:46:58 -0500

    added todo component and first route to todo for no path provided

--- grasp-app/src/app/app.routes.ts
+++ grasp-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // no path provided
+    {path:"", component: TodoComponent}
+];
