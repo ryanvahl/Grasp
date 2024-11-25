9bde78f9bb8bed06a37532fc77e93385014f40d9
Ryan <ryanvahl@gmail.com>
2024-11-25 04:39:09 -0500

    add dynamically creating exercises in program with dynamic adding sets and reps, does not save data yet

--- grasp-app/src/app/program-form/program-form.component.ts
+++ grasp-app/src/app/program-form/program-form.component.ts
@@ -16,10 +16,9 @@ import { Exercise } from '../models/exercise';
 export class ProgramFormComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder, private programService: ProgramService, private exerciseService: ExerciseService){}
-  
-
 
   exercises: Exercise[] = [];
+  // tableCount: number = 0;
 
   ngOnInit(): void {
     this.exercises = this.exerciseService.getExercises();
@@ -30,6 +29,16 @@ export class ProgramFormComponent implements OnInit {
   onSubmit() {
     if(this.programForm.valid) {
     console.log("program submitted!");
+    
+    const elements = document.querySelectorAll("#exercise-list");
+    console.log(elements[0]);
+
+    const select = elements[0].getElementsByTagName("select");
+    console.log(select);
+
+    elements.forEach((element) => {
+      console.log(element);
+    });
     }
   }
 
@@ -45,16 +54,45 @@ export class ProgramFormComponent implements OnInit {
     
     // attach to existing reps and set element and not the button
     let htmlElmentSibling = htmlElment.nextElementSibling;
-    if (htmlElmentSibling) {
-      htmlElmentSibling.innerHTML += 
-      `
-      <div>
-      <label>Reps</label>
-      <input type="number">
-      <label>Sets</label>
-      <input type="number">
-      </div>
-      `;    
+    console.log(htmlElmentSibling);
+    let lastTableRow = htmlElmentSibling?.lastChild;
+    console.log(lastTableRow);
+
+    // table data
+    let tableDataSet= document.createElement('td');
+    tableDataSet.textContent = "3";
+
+    let tableDataInputAndLabel = document.createElement('td');
+    
+    let label = document.createElement('label');
+    label.textContent = "Reps";
+    tableDataInputAndLabel.appendChild(label);
+
+    let input = document.createElement('input');
+    input.type = "number";
+    tableDataInputAndLabel.appendChild(input);
+
+    let tableDataButton = document.createElement('td');
+    let button = document.createElement('button');
+    button.type = "button";
+    button.textContent = "X";
+    tableDataButton.appendChild(button);
+    button.addEventListener("click", this.deleteSet);
+    
+    let nextRow = document.createElement('tr');
+    nextRow.appendChild(tableDataSet);
+    nextRow.appendChild(tableDataInputAndLabel);
+    nextRow.appendChild(tableDataButton);
+
+    htmlElmentSibling?.insertAdjacentElement('beforeend', nextRow);
     }
-  }
+
+    deleteSet(e: Event) {
+      console.log("clicked");  
+      let button = e.target as HTMLElement;
+      let row = button.closest('tr');
+      if(row) {
+        row.remove();
+      }
+    }
 }
